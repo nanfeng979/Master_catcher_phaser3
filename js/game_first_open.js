0c72e4be34ca56880f699b819748e7320d8608cb
nanfeng979 <306338401@qq.com>
2022-11-16 16:30:11 +0800

    增加 开始时需要点击开始才能进入游戏

--- js/game_first_open.js
+++ js/game_first_open.js
@@ -11,16 +11,26 @@ export let game_first_open = new Phaser.Class({
     preload : function () {
         this.load.image("game_cover", "./images/game_cover.png") // 引入游戏封面
         this.load.image("game_start_loading", "./images/game_start_loading.png") // 引入游戏加载页面
+        this.load.image("start", "./images/start.png")
     },
     create: function () {
         let _this = this
         this.add.image(canvasWidth / 2, canvasHeight / 2, "game_start_loading")
         let game_cover = this.add.image(canvasWidth / 2, canvasHeight / 2, "game_cover")
-        setTimeout(function() {
+        // setTimeout(function() {
+        //     game_cover.destroy()
+        // }, 2000)
+        // setTimeout(function(){
+        //     _this.scene.start("game_chose_level") 
+        // }, 5000)
+
+        let start = this.add.image(canvasWidth / 2, 670, "start").setScale(0.45).setInteractive()
+        start.on("pointerdown", () => {
             game_cover.destroy()
-        }, 2000)
-        setTimeout(function(){
-            _this.scene.start("char_chose") 
-        }, 5000)
+            start.destroy()
+            setTimeout(function(){
+                _this.scene.start("game_chose_level") 
+            }, 3000)
+        })
     }
 })
