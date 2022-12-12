b378f9e823c77abf0f1a86fa4bd1843ed119da56
nanfeng979 <306338401@qq.com>
2022-12-12 23:00:09 +0800

    一开头增加加载页面，避免黑屏

--- js/game_first_open.js
+++ js/game_first_open.js
@@ -1,7 +1,8 @@
 import {canvasWidth, canvasHeight} from "./index.js"
 
 
-let initCount = 280
+let initCount = 0
+let endCount = 420
 let count = initCount;
 let animObject
 
@@ -18,7 +19,7 @@ export let game_first_open = new Phaser.Class({
         this.load.image("game_start_loading", "./images/game_start_loading.png") // 引入游戏加载页面
         this.load.image("start", "./images/start.png")
 
-        for(var i = initCount; i < 421; i++)
+        for(var i = initCount; i < endCount + 1; i++)
         {
             if(i < 10)
             {
@@ -28,21 +29,24 @@ export let game_first_open = new Phaser.Class({
             } else {
                 this.load.image("b" + i, "./images/anim/3/游戏logo" + i + ".png")
             }
-        }
+        }   
     },
     create: function () {
+        // 加载动画挡住黑屏，并且再开始执行create()时删除加载动画
+        let load = document.getElementById("load")
+        let loadParent = load.parentElement
+        loadParent.removeChild(load)
+
         let _this = this
         this.add.image(canvasWidth / 2, canvasHeight / 2, "game_start_loading")
         let game_cover = this.add.image(canvasWidth / 2, canvasHeight / 2, "game_cover")
-        // setTimeout(function() {
-        //     game_cover.destroy()
-        // }, 2000)
-        // setTimeout(function(){
-        //     _this.scene.start("game_chose_level") 
-        // }, 5000)
 
         let start = this.add.image(canvasWidth / 2, 670, "start").setScale(0.45).setInteractive()
         start.on("pointerdown", () => {
+            // 动画加载完毕前不可以点击
+            if(count < endCount) {
+                return
+            }
             game_cover.destroy()
             start.destroy()
             setTimeout(function(){
@@ -55,7 +59,7 @@ export let game_first_open = new Phaser.Class({
         var timer = setInterval(function(){
             animObject.destroy()
             animObject = _this.add.image(canvasWidth / 2, canvasHeight / 2, "b" + count++)
-            if(count > 420)
+            if(count > endCount)
             {
                 clearInterval(timer)
             }
