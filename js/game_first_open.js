b3d939f8d5a9d279534c78e3ddb260853585c37d
nanfeng979 <306338401@qq.com>
2022-12-21 15:39:18 +0800

    修改加载页面

--- js/game_first_open.js
+++ js/game_first_open.js
@@ -55,7 +55,9 @@ export let game_first_open = new Phaser.Class({
         // let load = document.getElementById("load")
         // let loadParent = load.parentElement
         // loadParent.removeChild(load)
-        mp4.play()
+        setTimeout(function() {
+            mp4.play();
+        }, 2000)
         _this = this
         this.add.image(canvasWidth / 2, canvasHeight / 2, "game_start_loading")
         let game_cover = this.add.image(canvasWidth / 2, canvasHeight / 2, "game_cover")
